a8aae16d09fc886979c24ca85fb6018e073c094b
Debeshi Das <debeshidas3@gmail.com>
2025-06-07 17:45:42 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,9 @@
+height1=98
+height2=94
+height3=41
+height4=96
+height5=11
+sum=height1+height2+height3+height4+height5
+print("the sum is ", sum)
+avg=sum/5
+print("the average", avg)
---
+++ percentage.py
@@ -0,0 +1,8 @@
+maths=40
+science=70
+hindi=50
+english=60
+sum=maths+science+hindi+english
+print(sum)
+percentage=(sum/400)*100
+print(percentage)
