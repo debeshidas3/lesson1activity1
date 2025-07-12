173c8c39ea052d01195930a1ddd541770e8be340
Debeshi Das <debeshidas3@gmail.com>
2025-07-12 17:56:03 +0530

    Add files via upload

---
+++ divisible.py
@@ -0,0 +1,8 @@
+print("enter a numerator")
+numn=int(input())
+print("enter a denominator")
+numd=int(input())
+if numn%numd==0:
+   print("it is divisible")
+else:
+    print("it is not divisible") 
---
+++ operator.py
@@ -0,0 +1,6 @@
+name="alex"
+age=0
+if name=="alex" or name=="john" and age>=2:
+   print("welcome")
+else:
+    print("goodbye")     
