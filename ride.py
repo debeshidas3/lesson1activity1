1b9ed3cba6e8c02befbc50018a28a99ec9aee411
Debeshi Das <debeshidas3@gmail.com>
2025-08-02 17:25:32 +0530

    Add files via upload

---
+++ ride.py
@@ -0,0 +1,24 @@
+print("lets pick a ride")
+print("1.bike")
+print("2.car")
+ride=input("type 1 for bike,2 for car")
+if ride=="1":
+   print("you have chosen bike") 
+   print("choose your bike")
+   print("a.small bike")
+   print("b.big bike")
+   bike=input("type a or b")
+   if bike=="a":
+      print("you picked a small bike")
+   else:
+       print("you pick a big bike")
+if ride=="2":
+   print("you have chosen car") 
+   print("choose your car")
+   print("a.small car")
+   print("b.big car")
+   bike=input("type a or b")
+   if bike=="a":
+      print("you picked a small car")
+   else:
+       print("you pick a big car")          
