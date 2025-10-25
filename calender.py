2d39add8bf568332e36d3d81edb3a7c290270586
Debeshi Das <debeshidas3@gmail.com>
2025-10-25 14:22:10 +0530

    Add files via upload

---
+++ calender.py
@@ -0,0 +1,16 @@
+import calender
+import datetime 
+print("1.January")
+print("2.Febuary")
+print("3.march")
+print("4.April")
+print("5.May")
+print("6.June")
+print("7.July")
+print("8.August")
+print("9.Semtember")
+print("10.October")
+print("11.November")
+print("12.December")
+now=datetime.datetime.now()
+print("month and date is",now)
