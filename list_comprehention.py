d4421c96e4d7ddf3e9f3b70f8e07d194b2f8cacf
Debeshi Das <debeshidas3@gmail.com>
2025-11-29 17:21:59 +0530

    Add files via upload

---
+++ list_comprehention.py
@@ -0,0 +1,8 @@
+list=[1,3,5,7,9]
+list=[11,13,15,17,19]
+fruits = ['banana', 'watermelon', 'apple', 'coconut']
+    # Convert each fruit name to uppercase
+fruits_upper = []
+for fruit in fruits:
+    fruits_upper.append(fruit.upper())   
+    print("Fruits in capital letters:", fruits_upper)
