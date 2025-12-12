474e02814f9a81b500fb24dd7c48d18bede72c67
Debeshi Das <debeshidas3@gmail.com>
2025-12-12 20:28:03 +0530

    Add files via upload

---
+++ arrays_and_sets_activity.py
@@ -0,0 +1,9 @@
+setx = {"green","blue"}
+sety = {"blue","yellow"}
+print("Original sets elements:")
+print(setx)
+print(sety)
+print("\ntersection of two sets")
+setz = setx.intersection(sety)
+print(setz)
+
