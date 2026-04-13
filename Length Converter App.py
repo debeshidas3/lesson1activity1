2e1d106fb6d1abb72797c0768836e6493940514f
Debeshi Das <debeshidas3@gmail.com>
2026-04-13 21:58:37 +0530

    Add files via upload

---
+++ Length Converter App.py
@@ -0,0 +1,13 @@
+from tkinter import
+
+window = Tk()
+
+window.title("Length Converter App")
+window.geometry("400x400")
+
+def length converter():
+    length = int(entry_get())
+    length = inches
+    return = length * centimeters
+
+window.mainloop()
